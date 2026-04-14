510516629ed2a91236d51d9f4943ec2dd060ad92
Abdelrahman Essa <77458150+abdelrahmanessa@users.noreply.github.com>
2026-04-14 18:29:55 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,36 +1,79 @@
-    //asignment 3  
-    //Part1: Core Modules ( 1.5 Grades)
- //number 1 
-    const fs =require('fs');
-const zlib=require('zlib');
-const{pipeline}=require('stream');
+const http=require ('http');
+const fs = require('fs');
+const server = http.createServer((req,res)=>{
+    const  users= JSON.parse(fs.readFileSync('users.json','utf-8'))
+let{url,method} = req;
+// get  method
+if(url == '/user' && method == "GET"){
 
-    // const readStream=fs.createReadStream('./big.txt');
-    // readStream.on('data',(chunk)=>{
-    //     console.log(chunk.toString());
-    // });
-    // readStream.on('end',()=>{
-    //     console.log('finished');
-    // });
-    //____________________________________________________________________________      
-    //number 2
-//  const writeStream=fs.createWriteStream('./source.txt');
-//  const readstream1=fs.createReadStream('./dest.txt');
-//  readstream1.pipe(writeStream);
-//  writeStream.on('finish', () => {
-//     console.log('File copied using streams.');
-// });
-//_________________________________________________________________
-//number 3
-// pipeline(
-//     fs.createReadStream('./data.txt'),
-//     zlib.createGzip(),
-//     fs.createWriteStream('./data.txt.gz'),
-//     (error)=>{
-//         if(error){
-//             console.error('Pipeline failed:',error);
-//         }        else{
-//             console.log('Pipeline succeeded.');
-//         }
-//     }
-// )
+res.end(JSON.stringify(users));
+
+
+}
+// post method
+else if (url =='/user' && method == "POST"){
+
+let data;
+req.on('data',(chunk)=>{
+
+data=JSON.parse(chunk);
+
+})
+req.on('end',()=>{
+const id = users.length ? Math.max(...users.map(u => u.id)) + 1 : 1;
+let {name,age} = data;    
+users.push({id,name,age});
+fs.writeFileSync('users.json',JSON.stringify(users));
+    res.end('user added successfully');
+    console.log(users);
+    
+})
+} 
+// patch method
+else if (url == '/user' && method =="PATCH"){
+let id='';
+let data='';
+req.on('data',(chunk)=>{
+data=JSON.parse(chunk);
+const id = parseInt(url.split('/')[2]);})
+req.on('end',()=>{
+let userupdate =users.find(user => user.id == id)
+if(userupdate){
+    userupdate.name = data.name;
+    userupdate.age = data.age;
+    fs.writeFileSync('users.json',JSON.stringify(users));
+    res.end('user updated successfully');
+console.log(users);
+
+}
+else{
+res.end('user not found');
+}
+
+})
+}
+else if (url=='/user' && method == "DELETE"){
+ let id='';
+ req.on('data',(chunk)=>{
+const id = parseInt(url.split('/')[2]);
+ })
+ req.on('end',()=>{
+
+let userindex= users.findIndex(user => user.id ==id)
+if(userindex !=-1){
+    users.splice(userindex,1);
+    fs.writeFileSync('users.json',JSON.stringify(users));
+    res.end('user deleted successfully');
+    console.log(users);
+    
+}else{
+    res.end('user not found');
+}
+ })
+
+}
+});
+
+server.listen(3000,()=>{
+console.log('server is running on port 3000');
+})
